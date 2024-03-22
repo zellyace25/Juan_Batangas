d4ff211e34eee584979fc2a9251ea045a032d953
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-03-22 21:11:10 +0800

    Adding tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        lexend: ['Lexend', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
